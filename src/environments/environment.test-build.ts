b6a7fac75f5fa76a10669b46ba759ba4996f0fff
dk96hz <95834129+dk96hz@users.noreply.github.com>
2024-09-27 01:29:03 +0100

    Removed test environment

--- src/environments/environment.test-build.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: "https://bent-test-izPSjbq0zUa4UJCGCmO8ddMXhtOB.azurewebsites.net/api",
-  tpvUrl: "https://sis-t.redsys.es:25443/sis/realizarPago"
-};
