18b58a70efbb2cd2d0e5c402858eacfb98330bc3
dk96hz <95834129+dk96hz@users.noreply.github.com>
2024-09-27 01:45:54 +0100

    WB-73: added calendar subscription dialog to calendar

---
+++ src/app/features/calendar/components/calendar-subscription/calendar-subscription.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalendarSubscriptionComponent } from './calendar-subscription.component';
+
+describe('CalendarSubscriptionComponent', () => {
+  let component: CalendarSubscriptionComponent;
+  let fixture: ComponentFixture<CalendarSubscriptionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CalendarSubscriptionComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CalendarSubscriptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
