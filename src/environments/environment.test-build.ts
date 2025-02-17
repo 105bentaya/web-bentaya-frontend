ece212b1c5e3886397625cd999897cf65f8076ed
dk96hz <95834129+dk96hz@users.noreply.github.com>
2025-02-17 23:10:26 +0000

    Updated environment apiUrl

--- src/environments/environment.test-build.ts
+++ src/environments/environment.test-build.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "web-bentaya-test-g7cebcbxguf7hhhn.spaincentral-01.azurewebsites.net/api",
-  tpvUrl: "https://sis-t.redsys.es:25443/sis/realizarPago"};
+  apiUrl: "https://web-bentaya-test-g7cebcbxguf7hhhn.spaincentral-01.azurewebsites.net/api",
+  tpvUrl: "https://sis-t.redsys.es:25443/sis/realizarPago"
+};
