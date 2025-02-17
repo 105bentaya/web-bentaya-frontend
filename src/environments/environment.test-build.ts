0786e8803ca6c6e8f437012599c2f3407e9836e9
dk96hz <95834129+dk96hz@users.noreply.github.com>
2025-02-17 23:02:49 +0000

    Updated workflows and test environment

--- src/environments/environment.test-build.ts
+++ src/environments/environment.test-build.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "web-bentaya-test-g7cebcbxguf7hhhn.spaincentral-01.azurewebsites.net",
+  apiUrl: "web-bentaya-test-g7cebcbxguf7hhhn.spaincentral-01.azurewebsites.net/api",
   tpvUrl: "https://sis-t.redsys.es:25443/sis/realizarPago"};
