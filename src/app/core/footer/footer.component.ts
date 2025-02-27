6af6862187a43c458ad17d3666159eb9b1594fc4
dk96hz <95834129+dk96hz@users.noreply.github.com>
2025-02-27 00:48:08 +0000

    WB-79: updated footer and fixed home styles

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -2,13 +2,13 @@ import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {socialMediaButtons} from "../../shared/constant";
 import {DividerModule} from "primeng/divider";
-import {ButtonDirective, ButtonIcon, ButtonLabel} from "primeng/button";
+import {ButtonDirective, ButtonIcon} from "primeng/button";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  imports: [RouterLink, DividerModule, ButtonDirective, ButtonIcon, ButtonLabel]
+  imports: [RouterLink, DividerModule, ButtonDirective, ButtonIcon]
 })
 export class FooterComponent {
   protected readonly socialMediaButtons = socialMediaButtons;
